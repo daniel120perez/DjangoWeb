b6845c25efb304de9dc0137ad1faeb41a8a17881
Kotaro-Axel <axel_uxu@hotmail.com>
2020-12-10 23:44:52 -0600

    updated db credentials

--- backend/settings.py
+++ backend/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Webdb',
-        'USER': 'Webdb',
-        'PASSWORD': 'Perez120',
+        'USER': 'Webuserdb',
+        'PASSWORD': 'Web1234',
         'HOST': 'localhost',
         'PORT': '5432',
     } 
